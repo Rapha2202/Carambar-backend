265718fa5e1222d89add13882eeed2042a5fac7d
Raphael Foulon-Binet <raphael.foulonbinet@gmail.com>
2025-10-07 09:05:35 +0200

    Add CORS

--- src/app.js
+++ src/app.js
@@ -1,15 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
 import router from "./router.js";
 import { swaggerUi, specs } from "./config/swagger.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, {
-  customCss: '.swagger-ui .topbar { display: none }',
-  customSiteTitle: "Carambar API Documentation"
-}));
+const frontendUrl = process.env.FRONTEND_URL || "";
+app.use((req, res, next) => {
+  if (frontendUrl) {
+    res.setHeader("Access-Control-Allow-Origin", frontendUrl);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    customCss: ".swagger-ui .topbar { display: none }",
+    customSiteTitle: "Carambar API Documentation",
+  })
+);
 
 app.use("/api/v1", router);
 
